b46da5c1b064da6f67ebc31a843df6d412445ddc
MaxZhukoff <maksus298@gmail.com>
2023-03-31 18:23:40 +0300

    refactor: add SchedulingConfiguration

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/ApplicationConfig.java
@@ -2,21 +2,13 @@ package ru.tinkoff.edu.java.scrapper.configuration;
 
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.annotation.Validated;
 
 import java.time.Duration;
 
 @Validated
-@EnableScheduling
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(@NotNull String test, @NotNull Scheduler scheduler) {
     record Scheduler(Duration interval) {
-    }
-
-    @Bean
-    public long schedulerInterval() {
-        return scheduler.interval().toMillis();
     }
 }
