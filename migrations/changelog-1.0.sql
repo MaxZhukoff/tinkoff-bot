cda03ecf189455ceddb136c3221d5010ccd0f241
MaxZhukoff <62457503+MaxZhukoff@users.noreply.github.com>
2023-04-08 18:28:49 +0300

    feat: add database schema migrations

---
+++ migrations/changelog-1.0.sql
@@ -0,0 +1,23 @@
+--liquibase formatted sql
+
+--changeset maxzhukoff:1
+CREATE TABLE IF NOT EXISTS link
+(
+    id  BIGINT PRIMARY KEY,
+    url VARCHAR(255) NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+
+--changeset maxzhukoff:2
+CREATE TABLE IF NOT EXISTS chat
+(
+    id  BIGINT PRIMARY KEY
+);
+
+--changeset maxzhukoff:3
+CREATE TABLE IF NOT EXISTS link_chat
+(
+    link_id BIGINT NOT NULL REFERENCES link(id) ON DELETE CASCADE,
+    chat_id BIGINT NOT NULL REFERENCES chat(id) ON DELETE CASCADE,
+    PRIMARY KEY (link_id, chat_id)
+);
