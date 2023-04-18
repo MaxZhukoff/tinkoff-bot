4802c039af3b018a148ead1a7907d2d3af5baf5f
MaxZhukoff <62457503+MaxZhukoff@users.noreply.github.com>
2023-04-18 19:08:08 +0300

    feat: add interfaces and jdbc impl for chat and link services

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/controller/ChatController.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/controller/ChatController.java
@@ -6,23 +6,29 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.constraints.Min;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.tinkoff.edu.java.scrapper.dto.ApiErrorResponse;
+import ru.tinkoff.edu.java.scrapper.dto.controller.ApiErrorResponse;
+import ru.tinkoff.edu.java.scrapper.service.TgChatService;
 
 @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса", content = {
         @Content(
                 mediaType = "application/json",
                 schema = @Schema(implementation = ApiErrorResponse.class))
 })
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/tg-chat/{id}")
 public class ChatController {
+    private final TgChatService chatService;
+
     @Operation(summary = "Зарегистрировать чат")
     @ApiResponse(responseCode = "200", description = "Чат зарегистрирован")
     @PostMapping
     public ResponseEntity<Void> registerChat(@PathVariable @Min(0) Long id) {
-        return ResponseEntity.status(501).build();
+        chatService.register(id);
+        return ResponseEntity.status(200).build();
     }
 
     @Operation(summary = "Удалить чат")
@@ -36,6 +42,7 @@ public class ChatController {
     })
     @DeleteMapping
     public ResponseEntity<Void> deleteChat(@PathVariable @Min(0) Long id) {
-        return ResponseEntity.status(501).build();
+        chatService.unregister(id);
+        return ResponseEntity.status(200).build();
     }
 }
---
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/service/jdbc/JdbcTgChatService.java
@@ -0,0 +1,31 @@
+package ru.tinkoff.edu.java.scrapper.service.jdbc;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import ru.tinkoff.edu.java.scrapper.repository.JdbcTgChatRepository;
+import ru.tinkoff.edu.java.scrapper.service.TgChatService;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RequiredArgsConstructor
+@Service
+public class JdbcTgChatService implements TgChatService {
+    private final JdbcTgChatRepository tgChatRepository;
+
+    @Override
+    public void register(Long tgChatId) {
+        tgChatRepository.add(tgChatId);
+    }
+
+    @Override
+    public void unregister(Long tgChatId) {
+        try {
+            tgChatRepository.remove(tgChatId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResponseStatusException(NOT_FOUND, "Chat not found");
+        }
+
+    }
+}
